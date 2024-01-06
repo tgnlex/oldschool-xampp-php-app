26c16ee24b3ac9ac532e21f28e78ca69dd5ecdd6
tgnlex <alexanderwebber76@gmail.com>
2024-01-05 19:00:00 -0800

    navbar cleanup

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Navbar</a>
+    <a class="navbar-brand" href="index.php">MY PHP SERVER</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -9,9 +9,7 @@
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
-        </li>
+        
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
@@ -24,6 +22,11 @@
           </ul>
         </li>
       </ul>
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" href="login.php">Login</a>
+        </li>
+        </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
