12ccdf271969791b908776cbd46566b4b44a9618
tgnlex <alexanderwebber76@gmail.com>
2024-01-05 17:18:54 -0800

    initial commit

---
+++ pages/index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+ <?php include './templates/heading.php' ?>
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+     
+    </div>
+  </main>
+</div>
+<?php include './templates/imports.php' ?>
+</body>
+</html>
---
+++ partials/imports.php
@@ -0,0 +1,16 @@
+ <!---- Tailwind ---->
+ <script src="https://cdn.tailwindcss.com"></script>
+ <!--- PyScript --->
+ <script src="https://pyscript.net/releases/2023.11.1/core.js"></script> 
+ <!---- HTMX ----> 
+ <script src="https://unpkg.com/htmx.org@1.9.8"></script>  
+
+<!---- HyperScript ----->
+ <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
+
+ <!---- JQuery ---->
+ <script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous">
+</script>
