b4e19d8af754d5a0de942c3b7f888f7314fbcc13
tgnlex <alexanderwebber76@gmail.com>
2024-01-07 01:26:21 -0800

    setup root url redirect

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <link rel="stylesheet" href="./style/index.css" />
+  <title>...</title>
+  <meta http-equiv="refresh" content="0;url=http://localhost/website/pages/home.php"/>
+</head>
+<body>
+<h1>Loading...</h1>
+</body>
+</html>
