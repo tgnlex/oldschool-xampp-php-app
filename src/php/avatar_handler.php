e776b4e8eb7c564ae75e6bb5b87052d2b94fdd99
tgnlex <alexanderwebber76@gmail.com>
2024-01-10 00:01:37 -0800

    updated avatar handler

--- src/php/avatar_handler.php
+++ src/php/avatar_handler.php
@@ -6,14 +6,24 @@ $upload_success = 1;
 
 if (isset($_POST["submit"])) {
   $validate_avatar = getimagesize($_FILES["new_avatar"]["tmp_name"]);
-  if($validate_avatar !== false) {
-	echo "Image detected - " . $validate_avatar["mime"] . ".";
-	echo "Uploading Avatar...";
-	$upload_success = 1;
-  } else {
-	echo "Error: Please select a valid image file.";
-  	return $upload_success = 0; 
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+    echo "Sorry, only JPG andNGs are allowed files are allowed.";
+    $uploadOk = 0;
+  } else {  
+    if($validate_avatar !== false) {
+
+		// SUCCESS CODE GOES HERE //
+
+	  echo "Image detected - " . $validate_avatar["mime"] . ".";
+	  echo "Uploading Avatar...";
+	  $uploadOk = 1;
+
+		  // END OF SUCCESS CODE //
+
+	} else {
+	  echo "Error: Please select a valid image file.";
+  	  $uploadOk = 0; 
+	};
   }
-  
 }
 ?>
